e2e5fb3374abd4bc41d0417dd22328abfa76edc1
Zetaphor <zetaphor@hey.com>
2025-01-07 00:48:20 -0600

    New repo

---
+++ old_archiver/area.mjs
@@ -0,0 +1,145 @@
+import request from 'request';
+import fs from 'fs';
+import { headers } from "./utils.mjs";
+
+/**
+ * Get area id and key
+ * @param {string} areaValue
+ * @param {boolean} isName If false searches by area name isntead of areaId
+ * @returns {Promise}
+ */
+export function getAreaIdentifiers(areaValue, isName = true) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      'method': 'POST',
+      'url': 'http://app.anyland.com/area/load',
+      'headers': headers,
+    };
+
+    if (isName) options.form = { 'areaUrlName': areaName.replace(/[(\ )(')(,)(&)]/g, '') };
+    else options.form = { 'areaId': areaValue };
+    request(options, function (error, response) {
+      if (error) reject(error);
+      if (typeof response === 'undefined' || typeof response.body === 'undefined') reject('Missing response');
+      const areaData = JSON.parse(response.body);
+      if (typeof areaData.areaId === 'undefined' || typeof areaData.areaKey === 'undefined') reject('Undefined keys probably area name');
+      resolve({ id: areaData.areaId, key: areaData.areaKey, areaData: areaData });
+    });
+  });
+}
+
+/**
+ * Get subareas for a given area, including identifiers
+ * @param {string} areaId
+ * @returns {Promise}
+ */
+export function getSubAreas(areaId) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      'method': 'POST',
+      'url': 'http://app.anyland.com/area/getsubareas',
+      'headers': headers,
+      form: { 'areaId': areaId }
+    };
+    request(options, async function (error, response) {
+      if (error) reject(error);
+      if (typeof response === 'undefined' || typeof response.body === 'undefined') reject('Missing response');
+      try {
+        const areaData = JSON.parse(response.body);
+        if (typeof areaData.subAreas === 'undefined' || !areaData.subAreas.length) resolve([]);
+        let subAreas = [];
+        for (let i = 0; i < areaData.subAreas.length; i++) {
+          const identifiers = await getAreaIdentifiers(areaData.subAreas[i].id, false);
+          subAreas.push({
+            name: areaData.subAreas[i].name,
+            id: identifiers.id,
+            key: identifiers.key,
+            subArea: true,
+            parentId: areaId,
+            areaData: identifiers
+          });
+        }
+        resolve(subAreas);
+      } catch (e) {
+        console.error(e);
+        reject(e);
+      }
+    });
+  });
+}
+
+/**
+ * Gets an areas JSON from the CDN
+ * @param {string} areaId
+ * @param {string} areaKey
+ * @returns {Promise}
+ */
+function getAreaBundle(areaId, areaKey) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      'method': 'GET',
+      'url': `http://d26e4xubm8adxu.cloudfront.net/${areaId}/${areaKey}`,
+      'headers': headers
+    };
+    request(options, function (error, response) {
+      if (error) reject(error);
+      if (typeof response === 'undefined' || typeof response.body === 'undefined') {
+        reject('Missing response');
+      } else {
+        resolve(response.body);
+      }
+    });
+  });
+}
+
+/**
+ * Saves an area JSON bundle to disk
+ * @param {string} areaName
+ * @param {string} areaId
+ * @param {string} areaKey
+ * @param {string} bundle Area JSON
+ */
+function saveAreaBundle(areaName, areaId, areaKey, bundle) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(`areas/${areaName}__${areaId}_${areaKey}.json`, bundle, function (err) {
+      if (err) reject(err)
+      resolve();
+    });
+  });
+}
+
+function saveAreaData(areaName, areaId, areaKey, areaData) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(`areas/${areaName}__${areaId}_${areaKey}_areaData.json`, JSON.stringify(areaData), function (err) {
+      if (err) reject(err)
+      resolve();
+    });
+  });
+}
+
+/**
+ * Archives an area by getting its bundle and saving it to disk
+ * @param {string} areaName
+ * @param {string} areaId
+ * @param {string} areaKey
+ * returns {Promise}
+ */
+export function archiveArea(areaName, areaId, areaKey, areaData) {
+  return new Promise((resolve, reject) => {
+    saveAreaData(areaName, areaId, areaKey, areaData).then(() => {
+      getAreaBundle(areaId, areaKey).then((bundle) => {
+        saveAreaBundle(areaName, areaId, areaKey, bundle).then(() => {
+          resolve({ success: true, msg: `Successfully archived area (${areaName})` });
+        }).catch((err) => {
+          reject({ success: false, msg: `Failed to save file (${areaName}) - ${err}` });
+        });
+      }).catch((err) => {
+        reject({ success: false, msg: `Failed to get area bundle (${areaName}) - ${err}` });
+      });
+    }).catch((err) => {
+      reject({ success: false, msg: `Failed to save area data (${areaName}) - ${err}` });
+    });
+  }).catch((err) => {
+    reject({ success: false, msg: `Failed to get area identifiers (${areaName}) - ${err}`});
+  });
+}
---
+++ old_archiver/utils.mjs
@@ -0,0 +1,121 @@
+import fs from 'fs';
+import 'dotenv/config'
+import glob from 'glob';
+
+/**
+ * This is used by all network requests to mimic the Anyland client and include the session cookie
+ */
+export const headers = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'User-Agent': 'UnityPlayer/2018.1.0f2 (UnityWebRequest/1.0, libcurl/7.51.0-DEV)',
+  'Accept': '*/*',
+  'Accept-Encoding': 'identity',
+  'X-Unity-Version': '2018.1.0f2',
+  'Cookie': process.env.ANYLAND_COOKIE
+};
+
+/**
+ * Check if an area has been archived
+ * @param {string} areaName
+ * @param {string} areaId
+ * @param {string} areaKey
+ * @returns {boolean}
+ */
+export function isAreaArchived(areaName, areaId) {
+  let generalFiles = glob.sync(`areas/${areaName}__${areaId}_*.json`);
+  let areaDataFiles = glob.sync(`areas/${areaName}__${areaId}_*_areaData.json`);
+
+  return generalFiles.length > 0 && areaDataFiles.length > 0;
+}
+
+/**
+ * Log a failed download
+ * @param {string} areaName
+ * @param {string} areaId
+ * @param {string} areaKey
+ * @param {string} error
+ * @returns {Promise}
+ */
+export function logFailedArchive(areaName, areaId, areaKey, error) {
+  return new Promise((resolve, reject) => {
+    areaName = areaName.replace(/,/g, '');
+    const logData = `${areaName},${areaId},${areaKey},${error}\n`;
+    fs.appendFile(`failed_logs/failedDownloads.csv`, logData, function (err) {
+      if (err) reject(err)
+      resolve();
+    });
+  });
+}
+
+/**
+ * Rotate the failed download log
+ * Moves the old log to failedDownlods.N.csv where N is the number of old logs ie: failedDownloads.1.csv
+ */
+export function rotateFailedDownloadsLog() {
+  const basePath = 'failed_logs/failedDownloads';
+  const csvExtension = '.csv';
+  const csvPath = `${basePath}${csvExtension}`;
+  const headers = 'areaName,areaId,areaKey,error\n';
+
+  if (fs.existsSync(csvPath)) {
+      const contents = fs.readFileSync(csvPath, 'utf8');
+      if (contents.trim() !== "areaName,areaId,areaKey,error") {
+          let counter = 1;
+          while (fs.existsSync(`${basePath}.${counter}${csvExtension}`)) {
+              counter++;
+          }
+          fs.renameSync(csvPath, `${basePath}.${counter}${csvExtension}`);
+          fs.writeFileSync(csvPath, headers);
+      }
+  } else {
+      fs.writeFileSync(csvPath, headers);
+  }
+}
+
+/**
+ * Initialize the archive log csv
+ */
+export function createArchiveLog() {
+  if (!fs.existsSync('archived_world_list.csv')) {
+    const headers = 'areaName,areaId,areaKey,subArea,parentAreaId\n';
+    fs.writeFileSync('archived_world_list.csv', headers);
+  }
+}
+
+/**
+ * Add an archive log entry
+ * @param {string}} areaName
+ * @param {string}} areaId
+ * @param {string}} areaKey
+ * @param {string}} subArea
+ * @param {string}} parentId
+ */
+export function addArchiveLog(areaName, areaId, areaKey, subArea = false, parentId = '') {
+  areaName = areaName.replace(/,/g, '');
+  const row = `${areaName},${areaId},${areaKey},${subArea ? 'Y' : 'N'},${subArea ? parentId : '-'}\n`;
+  fs.appendFileSync('archived_world_list.csv', row);
+}
+
+/**
+ * Rotate the run output log
+ * Moves the old log to runOutput.N.csv where N is the number of old logs ie: runOutput.1.csv
+ */
+export function rotateRunOutputLog() {
+  const basePath = 'run_logs/runOutput';
+  const logExtension = '.log';
+  const logPath = `${basePath}${logExtension}`;
+
+  if (fs.existsSync(logPath)) {
+      const contents = fs.readFileSync(logPath, 'utf8');
+      if (contents.trim() !== "") {
+          let counter = 1;
+          while (fs.existsSync(`${basePath}.${counter}${logExtension}`)) {
+              counter++;
+          }
+          fs.renameSync(logPath, `${basePath}.${counter}${logExtension}`);
+          fs.writeFileSync(logPath, '');
+      }
+  } else {
+      fs.writeFileSync(logPath, '');
+  }
+}
